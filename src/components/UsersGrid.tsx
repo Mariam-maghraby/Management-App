db015d467ceadfc2dffe7a6ef7df061925bbf9bb
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-10-06 16:29:10 +0300

    fix: enabled prettier formatter

--- src/components/UsersGrid.tsx
+++ src/components/UsersGrid.tsx
@@ -1,28 +1,33 @@
-import { DataTable } from 'mantine-datatable';
-import users from '../data/users.json';
-import dayjs from 'dayjs';
+import { DataTable } from "mantine-datatable";
+import users from "../data/users.json";
+import dayjs from "dayjs";
 
 export default function UsersDataGrid() {
- 
-    return (
-        <DataTable
-          withBorder
-          borderRadius="md"
-          shadow="md"
-          highlightOnHover
-          fontSize="sm"
-          columns={[
-            { accessor: 'name' },
-            { accessor: 'userName' , title: 'User Name'},
-            { accessor: 'emailAddress', title: 'Email Address'},
-            { accessor: 'group' },
-            { accessor: 'status' },
-            { accessor: 'createdOn',
-            render: ({ createdOn }) => dayjs(createdOn).format('MMM D, YYYY'),
-          }
-          ]}
-          records={users}
-        />
-      );
-  
+  return (
+    <DataTable
+      withBorder
+      borderRadius="md"
+      shadow="md"
+      highlightOnHover
+      fontSize="sm"
+      minHeight={100}
+      columns={[
+        { accessor: "name" },
+        { accessor: "userName", title: "User Name" },
+        { accessor: "emailAddress", title: "Email Address" },
+        { accessor: "group" },
+        { accessor: "status" },
+        {
+          accessor: "createdOn",
+          render: ({ createdOn }) => dayjs(createdOn).format("MMM D, YYYY"),
+        },
+      ]}
+      defaultColumnProps={{
+        noWrap: true,
+        ellipsis: true,
+        titleSx: (theme) => ({ "&&": { color: theme.colors.gray[6] } }),
+      }}
+      records={users}
+    />
+  );
 }
--- src/components/UsersManagement.tsx
+++ src/components/UsersManagement.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import UsersDataGrid from './UsersGrid';
+import React from "react";
+import UsersDataGrid from "./UsersGrid";
+import { Paper } from "@mantine/core";
 
 function UsersManagement() {
-    return (
-        <>
+  return (
+    <>
+      <Paper shadow="xs" radius="xs" p="xl">
         <h3>Users Managment</h3>
         <UsersDataGrid />
-        </>
-    );
+      </Paper>
+    </>
+  );
 }
 
 export default UsersManagement;
