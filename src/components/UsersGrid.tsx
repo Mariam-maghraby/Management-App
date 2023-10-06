eb6b99287b8988786efec6f3e8071657ff483141
Mariam-maghraby <61349498+Mariam-maghraby@users.noreply.github.com>
2023-10-06 18:25:38 +0300

    feat: added Avatar to the name cell

--- src/components/UsersGrid.tsx
+++ src/components/UsersGrid.tsx
@@ -3,37 +3,66 @@ import users from "../data/users.json";
 import dayjs from "dayjs";
 import { User } from "../types/User";
 import { useState } from "react";
+import { Group, Paper, TextInput, Anchor, Avatar } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
 
 export default function UsersDataGrid() {
   const [selectedRecords, setSelectedRecords] = useState<User[]>([]);
 
   return (
-    <DataTable
-      withBorder
-      borderRadius="md"
-      shadow="md"
-      highlightOnHover
-      fontSize="sm"
-      minHeight={100}
-      columns={[
-        { accessor: "name" },
-        { accessor: "userName", title: "User Name" },
-        { accessor: "emailAddress", title: "Email Address" },
-        { accessor: "group" },
-        { accessor: "status" },
-        {
-          accessor: "createdOn",
-          render: ({ createdOn }) => dayjs(createdOn).format("MMM D, YYYY"),
+    <Paper my="xl" py="xl" withBorder radius="md"  shadow="md">
+      <Group>
+      <TextInput
+        icon={<IconSearch />}
+        placeholder="Search.."
+        // onChange={(event) => getSearchResultsWithTitle(event)}
+      />
+      {/* <Fieldset legend="Personal information">
+      <TextInput label="Your name" placeholder="Your name" />
+      <TextInput label="Email" placeholder="Email" mt="md" />
+    </Fieldset> */}
+    <Anchor href="https://mantine.dev/" target="_blank">
+      All Filters
+    </Anchor>
+      </Group>
+      
+      <DataTable
+        withBorder
+        highlightOnHover
+        fontSize="sm"
+        minHeight={100}
+        columns={[
+          { accessor: "name" ,
+          // render: ({ name }) => {
+          //   return(
+          //   <Avatar color="cyan" radius="xl">MK</Avatar>
+          //   {name}
+          //   )
+          // },
+          render: ({ name}) => (
+            <>
+            <Avatar color="cyan" radius="xl" size="sm">MK</Avatar>{name}
+            </>
+          ),
         },
-      ]}
-      defaultColumnProps={{
-        noWrap: true,
-        ellipsis: true,
-        titleSx: (theme) => ({ "&&": { color: theme.colors.gray[6] } }),
-      }}
-      records={users}
-      selectedRecords={selectedRecords}
-      onSelectedRecordsChange={setSelectedRecords}
-    />
+          { accessor: "userName", title: "User Name" },
+          { accessor: "emailAddress", title: "Email Address" },
+          { accessor: "group" },
+          { accessor: "status" },
+          {
+            accessor: "createdOn",
+            render: ({ createdOn }) => dayjs(createdOn).format("MMM D, YYYY"),
+          },
+        ]}
+        defaultColumnProps={{
+          noWrap: true,
+          ellipsis: true,
+          titleSx: (theme) => ({ "&&": { color: theme.colors.gray[6] } }),
+        }}
+        records={users}
+        selectedRecords={selectedRecords}
+        onSelectedRecordsChange={setSelectedRecords}
+      />
+    </Paper>
   );
 }
